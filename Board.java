7c63beebe03aec8d99f4adc8c6693ecb51bf7872
Uzui2012 <higginkg@egb080000031.lancs.local>
2020-03-06 16:27:59 +0000

    Added Driver method

--- Board.java
+++ Board.java
@@ -1,4 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Board
 {
-    
+    public Board(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel emptyLabel = new JLabel();
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
