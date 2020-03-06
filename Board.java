97a8d31d65557976b210117beef35213bf0415f7
Uzui2012 <higginkg@egb080000031.lancs.local>
2020-03-06 17:23:22 +0000

    Basic JFrame construction and square architecture

--- Board.java
+++ Board.java
@@ -3,12 +3,29 @@ import java.awt.*;
 
 public class Board
 {
+    private Square coordinates[][];
+
     public Board(){
-        JFrame frame = new JFrame();
+        coordinates = new Square[5][];      
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(5,5));
+        for(int i = 0; i < 5; i ++){
+            coordinates[i] = new Square[5]; //init squares
+            for(int  j = 0; j < 5; j++){
+                //use level.csv later
+                coordinates[i][j] = new Square(i, j, new JButton(new ImageIcon("images/GreenFrog.png")));
+                panel.add(coordinates[i][j].getButton());
+            }
+        }
+
+        
+        JFrame frame = new JFrame("Frame");
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel emptyLabel = new JLabel();
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-        frame.pack();
+        frame.setMinimumSize(new Dimension(750,750));
+        frame.setResizable(false);       
         frame.setVisible(true);
     }
+
 }
--- Square.java
+++ Square.java
@@ -1,4 +1,24 @@
+import javax.swing.*;
+
 public class Square
 {
+    private int x,y;
+    private JButton button;
+    public Square(int x, int y, JButton button){
+        this.x = x;
+        this.y = y;
+        this.button = button;
+    }
+
+    public JButton getButton(){
+        return this.button;
+    }
+
+    public int getX(){
+        return this.x;
+    }
     
+    public int getY(){
+        return this.y;
+    }
 }
